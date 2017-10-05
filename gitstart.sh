b590e723dbaf84a2bf72c30f0a3ad604a8097301
siwansiwan <32504467+siwansiwan@users.noreply.github.com>
2017-10-05 16:23:43 -0700

    Create gitstart.sh

---
+++ gitstart.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+echo ""
+echo "********************************************************************"
+echo "********************************************************************"
+echo ""
+echo "  Git quickstart process is starting ..."
+echo ""
+echo "********************************************************************"
+echo "********************************************************************"
+echo ""
+
+mkdir gittest
+cd gittest
+git clone https://github.com/siwansiwan/devops-pvt
+git remote add upstream https://github.com/ssiwan/devops
+git fetch upstream
+git checkout master
+git rebase upstream/master 
+git push -f origin master 

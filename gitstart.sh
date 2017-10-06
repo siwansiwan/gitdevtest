f4b6909f4507617efcea43123a23960f8775eb2a
siwansiwan <32504467+siwansiwan@users.noreply.github.com>
2017-10-05 22:55:12 -0700

    Update gitstart.sh

--- gitstart.sh
+++ gitstart.sh
@@ -10,7 +10,7 @@ echo "********************************************************************"
 echo "********************************************************************"
 echo ""
 
-rm -rf devops
+sudo rm -rf devops
 git clone git@github.com:siwansiwan/devops.git
 cd devops
 git remote add upstream git@github.com:ssiwan/devops.git
